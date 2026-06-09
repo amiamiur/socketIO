6e76ab862d646fc8aea6be173fd9ce35919e2966
Amiurr <145832953+amiamiur@users.noreply.github.com>
2026-06-09 10:21:50 +0300

    Добавлено разделение на сообщения разных пользователей

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express'); //imports
 const socketio = require('socket.io');
 
 const app = express();
@@ -13,6 +13,7 @@ io.on('connection', (socket) => {
 
     socket.on("sendMessage", (data) =>{
         const text = data.data;
-        io.sockets.emit("newMessage", `${text}`);
+        const id = data.socket_id
+        io.sockets.emit("newMessage", {text: `${text}`, id: id});
     })
  });
